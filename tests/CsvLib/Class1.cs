9d5f3972680f31acdad2e81b15814ee1673e5d4d
wk <somnuk.wk@gmail.com>
2020-12-11 23:33:23 +0700

    Refactor

---
+++ tests/CsvLib/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace MyLib {
+    [CsvGenerator(Template = "B.csv")]
+    public partial class Industry { }
+
+    public class Program {
+        public static void Main() {
+            var csv = File.ReadAllText("resource/csv/B.csv");
+            var data = IndustryLoader.Parse(csv);
+
+            foreach (var item in data) {
+                Console.WriteLine($"{item.Year} {item.VariableCategory,50}");
+            }
+        }
+    }
+}
